3a9ff7d8c65a2a0fc9a4d6ef940178a020e55cef
itshdog <haydenwestphal@gmail.com>
2023-03-08 11:08:35 -0600

    fixed edit storage

--- src/components/List.js
+++ src/components/List.js
@@ -1,9 +1,9 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import ListForm from './ListForm'
 import ListWrapper from './ListWrapper'
 
 function List() {
-    const [items, setItems] = useState(JSON.parse(localStorage.getItem('items')))
+    const [items, setItems] = useState(JSON.parse(localStorage.getItem('items')) || []);
 
     const addItem = item => {
         if(!item.text || /^\s*$/.test(item.text)) {
@@ -17,7 +17,7 @@ function List() {
 
         const newItems = [item, ...items]
 
-        newItems.sort((a, b) => (b.rank - a.rank));
+        // newItems.sort((a, b) => (b.rank - a.rank));
 
         setItems(newItems);
         localStorage.setItem('items', JSON.stringify(newItems));
@@ -28,6 +28,7 @@ function List() {
         const removeArr = [...items].filter(item => item.id !== e)
 
         setItems(removeArr);
+        localStorage.setItem('items', JSON.stringify(removeArr));
     };
 
     const editItem = (itemId, newItem) => {
@@ -39,14 +40,19 @@ function List() {
             console.log("Error: rank input is not valid. No changes made");
             return;
         }
-
-        setItems(prev => prev.map(e => (e.id === itemId ? newItem : e)));
-        localStorage.setItem('items', JSON.stringify(items));
+        // setItems(prev => prev.map(e => (e.id === itemId ? newItem : e)));
+        updateItems(itemId, newItem);
     };
+
+    function updateItems(itemId, newItem) {
+        const updatedItems = items.map(item => (item.id === itemId ? newItem : item));
+        setItems(updatedItems);
+        localStorage.setItem('items', JSON.stringify(updatedItems));
+    }
 
     return (
         <div>
-            <h1>Rankings</h1>
+            <h1>Ranked List</h1>
             <ListForm onSubmit={addItem} />
             <ListWrapper 
                 items={items} 
